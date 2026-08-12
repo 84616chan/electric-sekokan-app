13b9271874d574725d952bdff343bcb480d95e87
84616chan <chan84616@gmail.com>
2026-08-12 21:39:28 +0900

    Create page.js

---
+++ ​app/page.js
@@ -0,0 +1,10 @@
+Javascript 
+
+export default function Home() {
+  return (
+    <main style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>電気施工監理検定アプリ</h1>
+      <p>ここから学習アプリがスタートします！</p>
+    </main>
+  );
+}
